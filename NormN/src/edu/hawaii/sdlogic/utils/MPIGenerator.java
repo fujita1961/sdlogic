eb4c2d6bc819f4d446eb87c54f38cf6af1de28a1
Satoru Fujita <fujita_s@hosei.ac.jp>
2018-11-20 18:27:59 +0900

    Multiple division function to host computers was fixed

--- NormN/src/edu/hawaii/sdlogic/utils/MPIGenerator.java
+++ NormN/src/edu/hawaii/sdlogic/utils/MPIGenerator.java
@@ -1,17 +1,53 @@
 package edu.hawaii.sdlogic.utils;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Properties;
 
 public class MPIGenerator {
-	public static String templateFileName = "../props/TwoSamePointSimple.properties";
+	// public static String templateFileName = "../props/TwoSamePointSimple.properties";
+	public static String templateFileName = "../props/MultiPeaksLocalizedVariableLive.properties";
 	public static String outputFileName = "tmp/result";
 	public static String gifFileName = "tmp/gif";
 	public static String propsFile = "tmp/props";
-	public static String command = "java edu.hawaii.sdlogic.Main ";
+	public static String hostFile = "tmp/host";
+	public static String command = "java -cp . edu.hawaii.sdlogic.Main ";
+	public static String hostfileName = "/cluster/etc/openmpi/hostfile";
+	public static boolean mpiFlag = true;
 
 	public static void main(String[] args) {
+		List<String> listHostFile = new ArrayList<String>();
+		int hosts = 0;
+
+		if(mpiFlag) {
+			// hostFile generator
+			try {
+				FileReader fr = new FileReader(hostfileName);
+				BufferedReader reader = new BufferedReader(fr);
+
+				String line;
+
+				while((line = reader.readLine()) != null) {
+					String name = hostFile + "_" + hosts;
+					listHostFile.add(name);
+					PrintWriter pw = new PrintWriter(name);
+					pw.println(line);
+					hosts++;
+					pw.close();
+				}
+
+				reader.close();
+				fr.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
 		String template = null;
 		if(args.length >= 1) {
 			template = args[0];
@@ -52,7 +88,7 @@ public class MPIGenerator {
 
 		StringBuilder mpiCommand = new StringBuilder();
 		// mpiCommand.append("mpirun -hostfile /cluster/etc/openmpi/hostfile ");
-		
+
 
 		for(double d = 1.6; d < 1.8; d += 0.1, counter++) {
 			String propFile = propsFile + "_" + counter + ".properties";
@@ -66,8 +102,10 @@ public class MPIGenerator {
 				mpiCommand.append(": ");
 			}
 			*/
-			mpiCommand.append("mpirun -hostfile /cluster/etc/openmpi/hostfile ");
-			mpiCommand.append("-np 1 ");
+			if(mpiFlag) {
+				mpiCommand.append("mpirun -hostfile " + listHostFile.get(counter % listHostFile.size()));
+				mpiCommand.append(" -np 1 ");
+			}
 			mpiCommand.append(command);
 			mpiCommand.append(propFile);
 			mpiCommand.append(" & \n");
